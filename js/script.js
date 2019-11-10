849c3ab0160674a50ca4d954dc4c575ba41d9209
david withmore <davidwithmore422@gmail.com>
2019-11-10 09:19:11 +0800

    this is my second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-(function() {
-  const initializeAdminModal = function() {
+(() => {
+  const initializeAdminModal = () => {
     const modalContainer = document.querySelectorAll(".modalcontainer")[0];
     const formEditModal = document.querySelectorAll(".form-edit-container")[0];
     const formModalContainer = document.querySelectorAll(
@@ -12,7 +12,7 @@
     )[0];
     const formTitleEdit = document.querySelectorAll(".formtitle-edit")[0];
 
-    const removeModal = function() {
+    const removeModal = () => {
       modalContainer.style.display = "none";
       formTitleEdit.style.display = "none";
       if (formEditModal.style.display === "block") {
@@ -23,7 +23,7 @@
       }
     };
 
-    const showEditModal = function() {
+    const showEditModal = () => {
       modalContainer.style.display = "block";
       formTitleEdit.style.display = "block";
       formEditModal.style.display = "block";
@@ -31,7 +31,7 @@
       formDeleteModal.style.display = "none";
     };
 
-    const showDeleteModal = function() {
+    const showDeleteModal = () => {
       modalContainer.style.display = "block";
       formTitleEdit.style.display = "none";
       formEditModal.style.display = "none";
@@ -39,7 +39,7 @@
       formDeleteModal.style.display = "block";
     };
 
-    const deleteEntireModal = function() {
+    const deleteEntireModal = () => {
       const closeBtn = document.querySelector("#closebtn");
       closeBtn.addEventListener("click", removeModal, false);
     };
@@ -73,7 +73,7 @@
     }
 
     function initiateButtonControls() {
-      const removeData = function() {
+      const removeData = () => {
         modalContainer.style.display = "none";
         formDeleteModal.style.display = "none";
       };
@@ -92,7 +92,7 @@
     trigerModal();
   };
 
-  const initializeGraduateModal = function() {
+  const initializeGraduateModal = () => {
     const modalGraduatesContainer = document.querySelector("#modal-graduates");
     const viewBtn = document.querySelectorAll(".view-grad");
     const gradModalCloseBtn = document.querySelector("#closebtn-graduates");
@@ -115,10 +115,95 @@
     gradModalCloseBtn.addEventListener("click", hideGradInfo, false);
   };
 
-  if (document.querySelectorAll(".modalcontainer")[0]) {
-    initializeAdminModal();
-  }
-  if (document.querySelector("#modal-graduates")) {
-    initializeGraduateModal();
-  }
+  const initiateJobOfferModal = () => {
+    const jobOfferModal = document.querySelector("#modal-joboffer");
+    const jobOfferCloseBtn = document.querySelectorAll(".closebtnjoboffer");
+
+    const addnewJobOfferModal = document.querySelectorAll(".jobofferbtn")[0];
+    const addnewJobofferForm = document.querySelector("#addnewjoboffer");
+
+    const viewjobOfferModal = document.querySelectorAll(".view-joboffer");
+    const viewJobOfferInfo = document.querySelector("#viewjoboffer");
+
+    const editJobOfferModal = document.querySelectorAll(".edit-joboffer");
+    const editJobOfferInfo = document.querySelector("#editjoboffer");
+
+    const deleteJobOfferModal = document.querySelectorAll(".delete-joboffer");
+    const deleteJobOfferInfo = document.querySelector("#deletejoboffer");
+
+    const addJobOffer = () => {
+      jobOfferModal.style.display = "block";
+      viewJobOfferInfo.style.display = "none";
+      editJobOfferInfo.style.display = "none";
+      deleteJobOfferInfo.style.display = "none";
+      addnewJobofferForm.style.display = "block";
+    };
+    const closeJobOfferModal = () => {
+      jobOfferModal.style.display = "none";
+      addnewJobofferForm.style.display = "none";
+      viewJobOfferInfo.style.display = "none";
+      editJobOfferInfo.style.display = "none";
+      deleteJobOfferInfo.style.display = "none";
+    };
+
+    const viewJobOffer = () => {
+      jobOfferModal.style.display = "block";
+      viewJobOfferInfo.style.display = "block";
+      editJobOfferInfo.style.display = "none";
+      deleteJobOfferInfo.style.display = "none";
+      addnewJobofferForm.style.display = "none";
+    };
+    const editJobOffer = () => {
+      jobOfferModal.style.display = "block";
+      viewJobOfferInfo.style.display = "none";
+      editJobOfferInfo.style.display = "block";
+      deleteJobOfferInfo.style.display = "none";
+      addnewJobofferForm.style.display = "none";
+    };
+    const deleteJobOffer = () => {
+      jobOfferModal.style.display = "block";
+      viewJobOfferInfo.style.display = "none";
+      editJobOfferInfo.style.display = "none";
+      deleteJobOfferInfo.style.display = "block";
+      addnewJobofferForm.style.display = "none";
+    };
+
+    const initializeClickEvent = () => {
+      for (let i = 0, index = viewjobOfferModal.length; i < index; i++) {
+        viewjobOfferModal[i].addEventListener("click", viewJobOffer, false);
+      }
+
+      for (let i = 0, index = editJobOfferModal.length; i < index; i++) {
+        editJobOfferModal[i].addEventListener("click", editJobOffer, false);
+      }
+
+      for (let i = 0, index = deleteJobOfferModal.length; i < index; i++) {
+        deleteJobOfferModal[i].addEventListener("click", deleteJobOffer, false);
+      }
+
+      for (let i = 0, index = jobOfferCloseBtn.length; i < index; i++) {
+        jobOfferCloseBtn[i].addEventListener(
+          "click",
+          closeJobOfferModal,
+          false
+        );
+      }
+      addnewJobOfferModal.addEventListener("click", addJobOffer, false);
+    };
+    initializeClickEvent();
+  };
+
+  const ModalChecker = () => {
+    if (document.querySelectorAll(".modalcontainer")[0]) {
+      initializeAdminModal();
+    }
+    if (document.querySelector("#modal-graduates")) {
+      initializeGraduateModal();
+    }
+    if (document.querySelector("#modal-joboffer")) {
+      initiateJobOfferModal();
+    }
+  };
+
+  ModalChecker();
 })();
